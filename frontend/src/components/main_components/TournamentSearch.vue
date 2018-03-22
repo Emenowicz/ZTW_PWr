11ea41b3c2e50dfbdfb916080d4c594220b33253
Konrad Drozd <drozdkonrad@gmail.com>
2018-03-22 20:45:18 +0100

    Add missing files; change default main page.

---
+++ frontend/src/components/main_components/TournamentSearch.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h2>TOURNAMENT_SEARCH</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'TournamentSearch',
+    data() {
+      return {}
+    }
+  }
+</script>
