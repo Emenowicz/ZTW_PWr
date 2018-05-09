6adb1099098a449a9a3100a72ea1137d23c7f6a8
Konrad Drozd <konrad.drozd@tieto.com>
2018-05-09 20:47:44 +0200

    Improve tournament search. Data from backend mocked.

--- frontend/src/api/paths.js
+++ frontend/src/api/paths.js
@@ -1,4 +1,9 @@
+
+//TOURNAMENT
 export const TOURNAMENT = '/tournaments'
+export const TOURNAMENT_QUERY_PAGE = '/tournaments/find?q={0}?sd={1}?ed={2}?page={3}?size={4}' // {0} - tournament query, {1} - startDate, {2} - endDate, {3} - page number, {4} - page size
+export const JOIN_TOURNAMENT = 'tournaments/{0}/join' // {0} - tournament_id
+
+//USER
 export const USER = '/users'
 export const USER_TOURNAMENTS = '/users/{0}/ownedtournaments' // {0} - userId
-export const JOIN_TOURNAMENT = 'tournaments/{0}/join' // {0} - tournament_id
