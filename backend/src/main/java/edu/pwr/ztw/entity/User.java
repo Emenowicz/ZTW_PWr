14561e63207dd90e5d9fffc18746f5fb9d583af8
Konrad Drozd <konrad.drozd@tieto.com>
2018-05-11 14:25:35 +0200

    Remove auto-generating Id for User.

--- backend/src/main/java/edu/pwr/ztw/entity/User.java
+++ backend/src/main/java/edu/pwr/ztw/entity/User.java
@@ -12,7 +12,6 @@ import java.util.Set;
 @Entity
 public class User implements Serializable {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     private String id;
     @NotBlank
     private String name;
